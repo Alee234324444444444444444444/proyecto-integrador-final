3595878b00f8e78c31b41a21752216f4bcb27eb0
jayelsea <fuirner@gmail.com>
2025-02-04 23:02:47 -0500

    changes

--- server/hashPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function generarHash() {
-  const password = 'hola'; // Cambia esta contraseña por la que deseas cifrar
-  const hash = await bcrypt.hash(password, 10);
-  console.log('Password Hash:', hash);
-}
-
-generarHash();
